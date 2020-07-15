f61df3c4720449cc1dd984b216ecffff9a1f71a8
nethofelix <62725960+nethofelix@users.noreply.github.com>
2020-07-15 11:17:25 -0300

    commit
    Adição para ler Array dentro do objeto.

--- JsonToDataSet.pas
+++ JsonToDataSet.pas
@@ -1,5 +1,5 @@
 unit JsonToDataSet;
- {Alterada por Neto Félix  - 15/07/2020 - 01:44:55}
+ {Alterada por Neto Félix  - 14/07/2020 - 20:44:55}
 interface
 uses
  System.JSON,Data.DB, System.SysUtils, REST.Response.Adapter;
@@ -8,16 +8,17 @@ type
  TJsonToDataSet = class  Helper for TDataSet
  private
  public
-function JsonToDataset(aJson:String):TDataSet;
+function JsonToDataset(aJson:String;aElement:string = ''):TDataSet;
  end;
 
 implementation
 { TJsonToDelphi }
 
 
-function TJsonToDataSet.JsonToDataset(aJson:String):TDataSet;
+function TJsonToDataSet.JsonToDataset(aJson:String;aElement:string = ''): TDataSet;
 var
 FobjetoArray : TJSONArray;
+FElement     : TJSONArray;
 FObjeto      : TJSONObject;
 lValidacao   : string;
 vConv        : TCustomJSONDataSetAdapter;
@@ -30,20 +31,38 @@ begin
   lValidacao    := Copy(aJson,0,1);
   if lValidacao = '[' then
     begin
-    FobjetoArray  := System.Json.TJSONObject.ParseJSONValue(aJson) as System.JSON.TJSONArray;
-    vConv         := TCustomJSONDataSetAdapter.Create(Nil);
+      FobjetoArray  := System.Json.TJSONObject.ParseJSONValue(aJson) as System.JSON.TJSONArray;
+      vConv         := TCustomJSONDataSetAdapter.Create(Nil);
 
-     try
-      vConv.Dataset := self;
-      vConv.UpdateDataSet(FobjetoArray);
-    finally
-      vConv.Free;
-      FobjetoArray.Free;
+       try
+        vConv.Dataset := self;
+        vConv.UpdateDataSet(FobjetoArray);
+      finally
+        vConv.Free;
+        FobjetoArray.Free;
 
-    end;
-  end
+      end;
+     end
+
  else
  begin
+  if aElement <> '' then
+     begin
+       FObjeto       := System.Json.TJSONObject.ParseJSONValue(aJson) as System.JSON.TJSONObject;
+       FElement      := FObjeto.GetValue(aElement) as TJSONArray;
+       vConv         := TCustomJSONDataSetAdapter.Create(Nil);
+        try
+        vConv.Dataset := self;
+        vConv.UpdateDataSet(FElement);
+      finally
+        vConv.Free;
+        FObjeto.Free;
+
+      end;
+
+     end
+     else
+     begin
    FObjeto := System.Json.TJSONObject.ParseJSONValue(aJson) as System.JSON.TJSONObject;
    vConv   := TCustomJSONDataSetAdapter.Create(Nil);
   try
@@ -55,6 +74,7 @@ begin
   end;
 
  end;
+end;
 end;
 
 end.
